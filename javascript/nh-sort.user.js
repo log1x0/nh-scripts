02f6f45b228e7d69798a5d63424a1e0f5521e4b8
log1x0 <107345625+log1x0@users.noreply.github.com>
2026-01-05 07:31:09 +0100

    Update nh-sort.user.js
    match url adjusted

--- javascript/nh-sort.user.js
+++ javascript/nh-sort.user.js
@@ -5,12 +5,11 @@
 // @updateURL    https://github.com/log1x0/nh-scripts/raw/refs/heads/master/javascript/nh-sort.user.js
 // @downloadURL  https://github.com/log1x0/nh-scripts/raw/refs/heads/master/javascript/nh-sort.user.js
 // @supportURL   https://github.com/log1x0/nh-scripts/issues
-// @version      1.0.0
+// @version      1.0.1
 // @author       log1x0
-// @copyright    2024, log1x0
 // @license      MIT
 // @grant        none
-// @match        https://newheaven.nl/index.php
+// @match        https://newheaven.nl/index.php?*
 // @icon         https://newheaven.nl/favicon.ico
 // ==/UserScript==
 
